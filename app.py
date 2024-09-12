dc7e4c9ac662f2e18e8bcc4ace2388dbe0482819
anshikatandon <anshikatandon02@gmail.com>
2024-09-12 22:00:14 +0530

    analysis done

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 #to run open the terminal
 # streamlit run app.py
 import streamlit as st
-import pandas as py
+import pandas as pd
 import numpy as np
 import plotly.express as px
 
 #set pandas plotting backend to plotly
 pd.options.plotting.backend ="plotly"
 
+
 @st.cache_data
 def load_dataset():
-    return pd.read_csv("datasets/canada_clean.csv")
+    return pd.read_csv("datasets/canada.csv")
 
-st.title("Canada analysis")
+st.title("Canada Imigration Analysis")
 
 with st.spinner("loading data..."):
     df=load_dataset()
-    st.balloons()
+
+with st.expander("Show Dataset"):
+    st.dataframe(df)
+
+countrylist=df['country']
+selected_country=st.selectbox("select a country",countrylist)
+min_yr, max_yr = st.slider("Select Years", min_value=1980, max_value=2013, value=(1980, 2013))
+st.header(f"Country: {selected_country}")
+df = df.set_index('country')
+country = df.loc[selected_country, str(min_yr):str(max_yr)]
+fig = country.plot(kind='area')
+st.plotly_chart(fig)
+
+    
+
